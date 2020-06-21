0182024bd4b7bb7d1f6e98b60b32eddcafb971eb
Matt Dent <matt.dent@loblaw.ca>
2020-06-21 13:24:48 -0400

    Updates

---
+++ precache-manifest.8e2a777cce46a9aa8adf5875ed9ac8d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e299e8fe7ef35b21e570c9b325bf41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c360b53e8c9b5b8c04f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7780b3a42a1e21f999c9",
+    "url": "/static/js/2.5123d0d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5123d0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c360b53e8c9b5b8c04f",
+    "url": "/static/js/main.f381ce5d.chunk.js"
+  },
+  {
+    "revision": "b79902f8759dfbee8912",
+    "url": "/static/js/runtime-main.7d533776.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
